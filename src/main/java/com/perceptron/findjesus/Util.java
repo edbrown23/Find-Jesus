51c19ec78f25bda79e255da206e14d05f21ba0cf
Eric Brown <eric.d.brown23@gmail.com>
2012-08-08 16:50:58 -0500

    Removed incorrect license

--- src/main/java/com/perceptron/findjesus/Util.java
+++ src/main/java/com/perceptron/findjesus/Util.java
@@ -4,12 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 /**
- * *****************************************************************************
- * This program is the property of Innovative Software Engineering
- * It is confidential and proprietary to Innovative Software Engineering
- * and may not be copied, used or disclosed without the advance
- * written permission of Innovative Software Engineering.
- * *****************************************************************************
  * User: ebrown
  * Date: 8/8/12
  * Time: 4:39 PM
--- src/main/java/test.java
+++ src/main/java/test.java
@@ -3,12 +3,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
- * *****************************************************************************
- * This program is the property of Innovative Software Engineering
- * It is confidential and proprietary to Innovative Software Engineering
- * and may not be copied, used or disclosed without the advance
- * written permission of Innovative Software Engineering.
- * *****************************************************************************
  * User: ebrown
  * Date: 8/8/12
  * Time: 4:42 PM
